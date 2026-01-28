89f7ae78bc8568ae5dd9e81407ba02b377826708
Allstenna <mart2006anna@gmail.com>
2026-01-28 17:21:31 +0700

    change1

--- public_html/admin_orders.php
+++ public_html/admin_orders.php
@@ -44,11 +44,11 @@ $orders = $stmt->fetchAll();
         <tbody>
             <?php foreach ($orders as $order): ?>
             <tr>
-                <td><?= $order['order_id'] ?></td>
-                <td><?= $order['created_at'] ?></td>
+                <td><?= htmlspecialchars($order['order_id']) ?></td>
+                <td><?= htmlspecialchars($order['created_at']) ?></td>
                 <td><?= htmlspecialchars($order['email']) ?></td>
                 <td><?= htmlspecialchars($order['title']) ?></td>
-                <td><?= $order['price'] ?> ₽</td>
+                <td><?= htmlspecialchars($order['price']) ?> ₽</td>
             </tr>
             <?php endforeach; ?>
         </tbody>
